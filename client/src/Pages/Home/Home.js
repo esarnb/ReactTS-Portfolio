33bc5b205c7f7d22fbe85d7e6787aa5553c46761
esarnb <esarnb@gmail.com>
2019-12-26 15:48:09 -0500

    Setup Home page components with antD

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Main from "../../Components/Main/Main";
+import Navbar from "../../Components/Navbar/Navbar";
+import Footing from "../../Components/Footer/Footer";
+
+import { Layout } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 function Home() {
 	return (
-        <Main />
+		<Layout>
+			<Header><Navbar /></Header>
+			<Content><Main /></Content>
+			<Footer><Footing/></Footer>
+		</Layout>
 	);
 }
 
 export default Home;
+  
