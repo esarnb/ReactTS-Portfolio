3112110650c95458d318574fbf91af1583ee82ee
esarnb <esarnb@gmail.com>
2019-12-26 13:37:42 -0500

    Setup App.js to route components, added main component folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import NotFound from "./components/NotFound";
+import Main from "./pages/Main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Test
-        </a>
-      </header>
-    </div>
+    // {/* When a user visits the page, this will display the first ReactJS Component */}
+    <Router>
+      <React.Fragment>
+        {/* Depending on the webpage url, pick a page component to display */}
+        <Switch> 
+          <Route exact path="/" component={Main} />
+          <Route component={NotFound} />
+        </Switch>
+      </React.Fragment>
+    </Router>
   );
 }
 
