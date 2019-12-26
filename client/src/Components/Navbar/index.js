5e2644dd2007ecbd066b2f82253a1c94f91369a0
esarnb <esarnb@gmail.com>
2019-12-26 16:57:39 -0500

    Edited css heights for navbar, main, footer. Removed css and antd layout for Home.

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Layout, Menu} from 'antd';
-
+import "./index.css"
 // const { SubMenu } = Menu;
 const { Header} = Layout;
 
 function Navbar() {
 	return (
-        <Header className="header">
+        <Header className="Navbar">
         <div className="logo" />
-        <Menu theme="dark" mode="horizontal" style={{ lineHeight: '64px' }} >
+        <Menu theme="dark" mode="horizontal" style={{ lineHeight: '7.5vh' }} >
             <Menu.Item key="1">nav 1</Menu.Item>
             <Menu.Item key="2">nav 2</Menu.Item>
             <Menu.Item key="3">nav 3</Menu.Item>
--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -4,20 +4,13 @@ import Navbar from "../../Components/Navbar/";
 import Footing from "../../Components/Footer/";
 import "./index.css";
 
-import { Layout } from 'antd';
-
-const { Header, Footer, Content } = Layout;
-
 function Home() {
 	return (
-		<Layout>
-			<Header><Navbar /></Header>
-			<Content>
-				{/* <p> Hello World </p> */}
-				<Main />
-			</Content>
-			<Footer><Footing/></Footer>
-		</Layout>
+		<React.Fragment>
+			<Navbar />
+			<Main />
+			<Footing/>
+		</React.Fragment>
 	);
 }
 
