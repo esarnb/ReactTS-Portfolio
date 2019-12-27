0854195ed181ae3bbf9df73270f78636c91b6165
esarnb <esarnb@gmail.com>
2019-12-27 12:02:04 -0500

    Labeled tabs with profile icon

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Layout, Menu} from 'antd';
+import { Avatar, Icon, Layout, Menu} from 'antd';
 import "./index.css"
 // const { SubMenu } = Menu;
 const { Header} = Layout;
@@ -7,11 +7,13 @@ const { Header} = Layout;
 function Navbar() {
 	return (
         <Header className="Navbar">
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" style={{ lineHeight: '7.5vh' }} >
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} style={{ lineHeight: '7.5vh' }} >
+            <Avatar className="logo"src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+            <Menu.Item key="1">Home</Menu.Item>
+            <Menu.Item key="2">Projects</Menu.Item>
+            <Menu.Item key="3">ThreeJS</Menu.Item>
+            <Menu.Item key="4">Discord</Menu.Item>
+            <Menu.Item key="5" className="">Profile</Menu.Item>
         </Menu>
         </Header>
 	);
