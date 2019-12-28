b97f9472b0b144c1c51577d2500834fe1bb7b587
esarnb <esarnb@gmail.com>
2019-12-28 18:59:04 -0500

    Convert main conponent to store initial state, convert navbar to keep track of page user is on

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,23 @@ import NotFound from "./Components/NotFound";
 import Home from "./Pages/Home/index.js";
 import "./App.css";
 
-function App() {
-  return (
-    // {/* When a user visits the page, this will display the first ReactJS Component */}
-    <Router>
+class App extends React.Component {
+  state = {
+    test: 1
+  }
+
+  render() {
+    return (  
+      <Router>
       <React.Fragment>
-        {/* Depending on the webpage url, pick a page component to display */}
         <Switch> 
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={<Home />} />
           <Route component={NotFound} />
         </Switch>
       </React.Fragment>
     </Router>
-  );
+    )
+  }
 }
 
 export default App;
--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -1,17 +1,55 @@
 import React from "react";
-import Main from "../../Components/Main/";
+
 import Navbar from "../../Components/Navbar/";
 import Footing from "../../Components/Footer/";
+
+import Main from "../../Components/Main/";
+import Projects from "../../Components/Projects/";
+import ThreeJS from "../../Components/ThreeJS/";
+import Discord from "../../Components/Discord/";
+import Profile from "../../Components/Profile/";
+
 import "./index.css";
 
-function Home() {
-	return (
-		<React.Fragment>
-			<Navbar />
-			<Main />
-			<Footing/>
-		</React.Fragment>
-	);
+class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { page: 1, test: this.props.test };
+	}
+
+	switchPage = (newPage) => {
+		this.setState({page: newPage})
+	}
+
+	determinePage = () => {
+		switch(this.state.page) {
+			case 2:
+				return <Projects />
+				break;
+			case 3:
+				return <ThreeJS />
+				break;
+			case 4:
+				return <Discord />
+				break;
+			case 5:
+				return <Profile />
+				break;
+			default:
+				return <Main />
+				break;
+		}
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<Navbar page={this.state.page} updatePage={switchPage}/>
+				{this.determinePage()}
+				<Footing/>
+			</React.Fragment>
+		)
+	};
 }
 
 export default Home;
