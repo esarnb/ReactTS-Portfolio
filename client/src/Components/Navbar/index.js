cbd714be9006ab2a835702051947957940a34e9d
esarnb <esarnb@gmail.com>
2019-12-28 20:44:56 -0500

    Positioned theme switch to right justify

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -18,14 +18,15 @@ class Navbar extends React.Component {
                 <Menu theme={this.props.theme} mode="horizontal" defaultSelectedKeys={['1']} style={{ lineHeight: '5vh' }} >
                     <Avatar className="logo"src="http://bestanimations.com/Nature/Fire/Flames/single-burning-flame-close-up-animated-gif-image.gif" />
                     <Menu.Item key="Spacer" />
+
                     <Menu.Item key="1" onClick={(res) => this.props.updatePage(res.key)}> Home </Menu.Item>
                     <Menu.Item key="2" onClick={(res) => this.props.updatePage(res.key)}> Projects </Menu.Item>
                     <Menu.Item key="3" onClick={(res) => this.props.updatePage(res.key)}> ThreeJS </Menu.Item>
                     <Menu.Item key="4" onClick={(res) => this.props.updatePage(res.key)}> Discord </Menu.Item>
                     <Menu.Item key="5" onClick={(res) => this.props.updatePage(res.key)}> Profile </Menu.Item>
-                    <Menu.Item key="Spacer" />
-                    <Switch checked={this.props.theme === 'dark'} onChange={this.changeTheme} checkedChildren="dark" unCheckedChildren="light" />
+                    
                 </Menu>
+                    <Switch className="Switcher" checked={this.props.theme === 'dark'} onChange={this.changeTheme} checkedChildren="dark" unCheckedChildren="light" />
             </Header>
         );
     }
