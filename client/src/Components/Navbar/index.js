ce0ff931ac078e7bb48b10398fac3247bc012e0c
esarnb <esarnb@gmail.com>
2019-12-28 19:50:42 -0500

    Insert theme switch

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -1,27 +1,39 @@
 import React from "react";
-import { Avatar, Layout, Menu} from 'antd';
+import { Avatar, Layout, Menu, Switch} from 'antd';
 import "./index.css"
 // const { SubMenu } = Menu;
-const { Header} = Layout;
+const { Header } = Layout;
 
-function Navbar(props) {
-    function test(obj, cb) {
-        console.log(obj.key);   
-        cb(obj.key)
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { theme: "Dark", current: "1" }
     }
-    return (
-        <Header className="Navbar">
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} style={{ lineHeight: '5vh' }} >
-                <Avatar className="logo"src="http://bestanimations.com/Nature/Fire/Flames/single-burning-flame-close-up-animated-gif-image.gif" />
-                <Menu.Item key="0" />
-                <Menu.Item key="1" onClick={(res) => test(res, props.updatePage)}> Home </Menu.Item>
-                <Menu.Item key="2" onClick={(res) => test(res, props.updatePage)}> Projects </Menu.Item>
-                <Menu.Item key="3" onClick={(res) => test(res, props.updatePage)}> ThreeJS </Menu.Item>
-                <Menu.Item key="4" onClick={(res) => test(res, props.updatePage)}> Discord </Menu.Item>
-                <Menu.Item key="5" onClick={(res) => test(res, props.updatePage)}> Profile </Menu.Item>
-            </Menu>
-        </Header>
-	);
+
+    changeTheme = value => {
+        this.setState({
+            theme: value ? 'dark' : 'light',
+        });
+    };
+    
+    
+
+    render() {
+        return (
+            <Header className="Navbar">
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} style={{ lineHeight: '5vh' }} >
+                    <Avatar className="logo"src="http://bestanimations.com/Nature/Fire/Flames/single-burning-flame-close-up-animated-gif-image.gif" />
+                    <Menu.Item key="Spacer" />
+                    <Menu.Item key="1" onClick={(res) => this.props.updatePage(res.key)}> Home </Menu.Item>
+                    <Menu.Item key="2" onClick={(res) => this.props.updatePage(res.key)}> Projects </Menu.Item>
+                    <Menu.Item key="3" onClick={(res) => this.props.updatePage(res.key)}> ThreeJS </Menu.Item>
+                    <Menu.Item key="4" onClick={(res) => this.props.updatePage(res.key)}> Discord </Menu.Item>
+                    <Menu.Item key="5" onClick={(res) => this.props.updatePage(res.key)}> Profile </Menu.Item>
+                    <Switch checked={this.state.theme === 'dark'} onChange={this.changeTheme} checkedChildren="Dark" unCheckedChildren="Light" />
+                </Menu>
+            </Header>
+        );
+    }
 }
 
 export default Navbar;
