15576b378a28990cb3af1a10038dbd91bdd9e79a
esarnb <esarnb@gmail.com>
2019-12-30 17:12:50 -0500

    Added cards structure for main page

--- client/src/Components/Cards/index.js
+++ client/src/Components/Cards/index.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Card,  Col, Row, Icon, Avatar } from 'antd';
+import "./index.css"
 
-function Cards() {
+const { Meta } = Card;
+
+function Cards(props) {
+	
 	return (
-		<div className="Cards">
-			
+		<div className="Cards" style={{ background: '#ECECEC', padding: '30px' }}>
+			<Row gutter={16}>
+				{/* {props.list.length ? props.list : <Card key="card-placehold" title="title" bordered={false}>placeholder</Card>} */}
+				{props.list.map((obj,index) => { return <Col span={8}>
+					<Card key={`${obj.key}-${index}`} style={{ width: obj.styleWidth }} 
+						cover={ <img alt={obj.cover.alt} src={obj.cover.src}/> }
+						actions={[ <Icon type="setting" key="setting" /> ]}>
+						<Meta avatar={<Avatar src={obj.avatar} />} 
+						title={obj.title} description={obj.desc} />
+					</Card>
+				</Col> })}
+			</Row>
 		</div>
 	);
 }
--- client/src/Components/Main/index.js
+++ client/src/Components/Main/index.js
@@ -1,12 +1,47 @@
 import React from "react";
 import "./index.css";
+import Cards from "../Cards";
 
 function Main(props) {
 	return (
 		<div className={`Main main-${props.theme}`}>
             <p className="Center Large">Welcome Home.</p>
+			<br /><br /><br />
+			<Cards list={[
+				{
+					key:"test",
+					styleWidth: 300,
+					cover: { alt: "pic",  src: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" },
+					action: { link: "" },
+					avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+					title: "title",
+					desc: "desc"
+
+				},
+				{
+					key:"test",
+					styleWidth: 300,
+					cover: { alt: "pic",  src: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" },
+					action: { link: "" },
+					avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+					title: "title",
+					desc: "desc"
+
+				},
+				{
+					key:"test",
+					styleWidth: 300,
+					cover: { alt: "pic",  src: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" },
+					action: { link: "" },
+					avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+					title: "title",
+					desc: "desc"
+
+				}
+			]} />
 		</div>
 	);
 }
+
 
 export default Main;
