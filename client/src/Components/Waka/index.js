a7c73f9af6973e7942111f8d78072e1e98b31b76
esarnb <esarnb@gmail.com>
2020-01-01 12:37:58 -0500

    size images to fit both, not breaking when page small

--- client/src/Components/Waka/index.js
+++ client/src/Components/Waka/index.js
@@ -3,8 +3,8 @@ import React from "react";
 function Waka(props) {
 	return (
 		<div className="Waka">
-			<figure style={{width: "100vh", float: "left", marginRight: "2vh"}}><embed src="https://wakatime.com/share/@Esarnb/14b0ec8e-03c4-46f3-a429-a1d9d9cee067.svg"></embed></figure>
-			<figure style={{width: "100vh", float: "left"}}><embed src="https://wakatime.com/share/@Esarnb/9e0a7158-647f-43f5-b178-582c0c4ac105.svg"></embed></figure>
+			<figure style={{width: "45%", float: "left", marginRight: "2vh"}}><embed src="https://wakatime.com/share/@Esarnb/14b0ec8e-03c4-46f3-a429-a1d9d9cee067.svg"></embed></figure>
+			<figure style={{width: "45%", float: "left"}}><embed src="https://wakatime.com/share/@Esarnb/9e0a7158-647f-43f5-b178-582c0c4ac105.svg"></embed></figure>
 		</div>
 	);
 }
