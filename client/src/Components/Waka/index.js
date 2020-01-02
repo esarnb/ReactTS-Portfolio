4bd5edf3cf327d1939c0d7811b08c74427ae4c49
esarnb <esarnb@gmail.com>
2020-01-01 19:17:00 -0500

    overflow auto to all pages will make header/footer sticky; undid most edits on components

--- client/src/Components/Waka/index.js
+++ client/src/Components/Waka/index.js
@@ -3,8 +3,8 @@ import React from "react";
 function Waka(props) {
 	return (
 		<div className="Waka" >
-			<figure style={{width: "50%", float: "left", marginRight: "2vh"}}><embed src="https://wakatime.com/share/@Esarnb/14b0ec8e-03c4-46f3-a429-a1d9d9cee067.svg"></embed></figure>
-			<figure style={{width: "50%", float: "left"}}><embed src="https://wakatime.com/share/@Esarnb/9e0a7158-647f-43f5-b178-582c0c4ac105.svg"></embed></figure>
+			<figure style={{width: "49%", float: "left", marginRight: "2vh"}}><embed src="https://wakatime.com/share/@Esarnb/14b0ec8e-03c4-46f3-a429-a1d9d9cee067.svg"></embed></figure>
+			<figure style={{width: "49%", float: "left"}}><embed src="https://wakatime.com/share/@Esarnb/9e0a7158-647f-43f5-b178-582c0c4ac105.svg"></embed></figure>
 		</div>
 	);
 }
