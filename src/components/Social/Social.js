f7cccd2ee0a09bb08e6ad62e9376825a2f2c6b2e
esarnb <esarnb@gmail.com>
2021-02-15 04:04:43 +0000

    Move unused files, social tag coloring

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -1,26 +1,26 @@
 import React, { Fragment } from "react"
-import { Tag } from "@chakra-ui/react"
-import { MdSettings } from '@chakra-ui/icons'
+import {
+    Tag,
+    TagLabel,
+    TagLeftIcon,
+    TagRightIcon,
+    TagCloseButton,
+  } from "@chakra-ui/react"
+import { BellIcon } from '@chakra-ui/icons'
 import "./Social.css";
 
 export default function Social() {
     return (
         <Fragment>
-              <Tag size="md" key="github" variant="solid" colorScheme="teal" icon={<GithubOutlined />} className="sMedia" color="#000"> 
-                    <a href="https://github.com/esarnb" target="_blank" rel="noreferrer">Github </a>
-                    <TagLabel>x</TagLabel>
-                    <TagRightIcon as={MdSettings} />
-                </Tag>
-              <Tag size="md" key="linkedin" variant="solid" colorScheme="teal" icon={<LinkedinOutlined />} className="sMedia" color="#0A66C2"> 
-                    <a href="https://www.linkedin.com/in/esarnb" target="_blank" rel="noreferrer"> LinkedIn </a>
-                    <TagLabel>x</TagLabel>
-                    <TagRightIcon as={MdSettings} />
-                </Tag>
-              <Tag size="md" key="medium" variant="solid" colorScheme="teal" icon={<MediumOutlined />} className="sMedia" color="#222"> 
-                    <a href="https://medium.com/@esarnb" target="_blank" rel="noreferrer"> Medium </a>
-                    <TagLabel>x</TagLabel>
-                    <TagRightIcon as={MdSettings} />
-                </Tag>
+            <Tag size="md" key="github" variant="solid" colorScheme="teal" className="sMedia" style={{backgroundColor: "#000"}} color="#fff"> 
+                <a href="https://github.com/esarnb" target="_blank" rel="noreferrer">Github </a>
+            </Tag>
+            <Tag size="md" key="linkedin" variant="solid" colorScheme="teal" className="sMedia" style={{backgroundColor: "#0A66C2"}} color="#fff"> 
+                <a href="https://www.linkedin.com/in/esarnb" target="_blank" rel="noreferrer"> LinkedIn </a>
+            </Tag>
+            <Tag size="md" key="medium" variant="solid" colorScheme="teal" className="sMedia" style={{backgroundColor: "#222"}} color="#fff"> 
+                <a href="https://medium.com/@esarnb" target="_blank" rel="noreferrer"> Medium </a>
+            </Tag>
         </Fragment>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from "@chakra-ui/react";
 
 import {createStore} from 'redux';
@@ -8,7 +8,7 @@ import {Provider} from 'react-redux';
 import allReducer from "./store/reducers";
 
 import App from './App';
-import './index.css';
+// import './index.css';
 
 const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
@@ -25,4 +25,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
