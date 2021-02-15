1c81f70b5c26c91bf1a487f84fe6c18017822830
esarnb <esarnb@gmail.com>
2021-02-15 05:52:44 +0000

    Nav btn remove underline and click box shadow, align to login btn

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,14 +7,14 @@ import "./Navbar.css";
 
 function Navbar() {
   return (
-    <Box id="nav-box">
-      <Tabs>
-        <TabList>
-          <Tab> <NavLink to="/">Home</NavLink> </Tab>
-          <Tab> <NavLink to="/github">Github</NavLink> </Tab>
-          <Tab> <NavLink to="/threejs">ThreeJS</NavLink> </Tab>
-          <Tab> <NavLink to="/config">Redux</NavLink> </Tab>
-          <Tab> <Login className="floatRight" /> </Tab>
+    <Box id="nav-box"  className="removeBoxBorder">
+      <Tabs className="removeBoxBorder">
+        <TabList  className="removeBoxBorder">
+           <NavLink style={{ outline: 'none' }} className="navlink" to="/"><Tab className="removeBoxBorder"> Home </Tab> </NavLink>
+           <NavLink style={{ outline: 'none' }} className="navlink" to="/github"><Tab className="removeBoxBorder"> Github </Tab> </NavLink>
+           <NavLink style={{ outline: 'none' }} className="navlink" to="/threejs"><Tab className="removeBoxBorder"> ThreeJS </Tab> </NavLink>
+           <NavLink style={{ outline: 'none' }} className="navlink" to="/config"><Tab className="removeBoxBorder"> Redux </Tab> </NavLink>
+           <Tab className="removeBoxBorder"> <Login style={{ outline: 'none' }} className="floatRight" /> </Tab>
           <span className="moveRight"> 
             <Avatar className="floatRight" name="Kent Dodds" src="https://bit.ly/kent-c-dodds" />
           </span>
