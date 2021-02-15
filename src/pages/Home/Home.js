75df6a34b6e10e180239dfae4e2f308891ee0b4c
esarnb <esarnb@gmail.com>
2021-02-15 03:54:29 +0000

    Social links in tags

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,15 @@
 import React, {Fragment} from "react"
 import { Container } from "@chakra-ui/react";
+import Social from "../../components/Social/Social";
 import "./Home.css";
 
 function Home() {
   return (
     <Container id="home-container">
         Home
+        <br />
+        <Social />
+
     </Container>
   )
 }
