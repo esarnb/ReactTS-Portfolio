c51bfcd93805435964efd94078c2f101938c5969
esarnb <esarnb@gmail.com>
2022-04-10 23:57:14 +0000

    expose endpoint

--- src/Pages/Discord/Discord.tsx
+++ src/Pages/Discord/Discord.tsx
@@ -1,13 +1,33 @@
 import { Helmet } from "react-helmet";
+import { useEffect, useState } from "react";
 import "./Discord.css";
+
 function Discord() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+
+    fetch("http://localhost:3434/discord/").then(res => {
+      console.log("Status: ", res.status);
+      
+      return res.json()
+    }).catch(err => console.error(err))
+    .then(data => {
+      setIsLoading(false);
+      setData(data);
+    });
+  }, []);
+
   return (
     <>
-       <Helmet>
+      <Helmet>
         <title>Discord</title>
         <meta name="description" content="My Discord page through Helmet" />
       </Helmet>
-      <div>Discord</div>
+      <p>
+        {JSON.stringify(data)}
+      </p>
     </>
   );
 }
