9c0a63b3e912ed75d8c2fddc2d39884903f48903
esarnb <esarnb@gmail.com>
2022-04-14 18:04:19 +0000

    same-page oauth, same-page API backend fetch user object

--- src/Pages/Discord/Discord.tsx
+++ src/Pages/Discord/Discord.tsx
@@ -1,92 +1,43 @@
 import { Helmet } from "react-helmet";
 import { useEffect, useState } from "react";
+import { useSearchParams } from 'react-router-dom'
+import {  userObj  } from "../../Interfaces/Discord";
+import DiscordUser from "../../Components/DiscordUser/DiscordUser";
+import DiscordBot from "../../Components/DiscordBot/DiscordBot";
 import "./Discord.css";
 
-interface command {
-  help: {
-    desc: string,
-    guild: boolean, 
-    locked: boolean,
-    name: string, 
-    owner: boolean, 
-    type: string,
-    usage: string,
-  }
-}
+export default function Discord() {
 
-interface botData {
-  user: any, // discord User Object
-  users: number,
-  servers: number,
-  commands: any // command Object
-}
+    const [searchParams] = useSearchParams();
+    const code = searchParams.get('code');
 
-var initial = {
-  user: {
-    tag: ""
-  },
-  users: 0,
-  servers: 0,
-  commands: []
-}
+    const [userData, setUserData] = useState<userObj>();
+    const oauthURL = "https://discord.com/api/oauth2/authorize?client_id=343831827963838475&redirect_uri=https%3A%2F%2Fesarnb.com%2Fdiscord&response_type=code&scope=identify";
 
-function Discord() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [bot, setBot] = useState<botData>(initial);
-
-  useEffect(() => {
-    // Abort Request if local API is down, ignore browser based timeouts just for this API
-    const controller = new AbortController()
-
-    // 5 second timeout:
-    const timeoutId = setTimeout(() => controller.abort(), 5000)
-
-    fetch("https://bot.esarnb.com/discord", { signal: controller.signal })
-      .then(res => {
-        // completed request before timeout fired
-        
-        clearTimeout(timeoutId) // timeout the request, not the response
-        console.log("Status: ", res.status);
-        return res.json()
-      })
-      .then(bot => {
-        setBot(bot);
-        console.log(bot);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      })
-      .catch(err => {
-        console.error(err);
-        initial.user.tag = "Offline."
-      });
-  }, []);
-
-  return (
-    <>
-      <Helmet>
-        <title>Discord</title>
-        <meta name="description" content="My Discord page through Helmet" />
-      </Helmet>
-      <div style={{textAlign: "center", justifyContent: "center"}}>
-        { isLoading ? 
-          <>
-            Fetching Bot data...
-          </> : <>
-
-            {
-              (bot.user.tag && (bot.user.tag !== "Offline.") ) 
-              ? <div>
-                  {bot.user.tag} is now online! Serving {bot.users} users and {bot.servers} servers.
-                </div> 
-              : <p>Bot is offline.</p>
-            }
-
-          </>
+    useEffect(() => {
+      console.log("CODE", code);
+      if (code) {
+            fetch("https://api.esarnb.com/discord?code=" + code).then((res) => res.json()).then((data: userObj) => {
+                console.log("DATA", data);
+                setUserData(data);
+            }).catch(err => console.error(err));
         }
-      </div>
-    </>
-  );
+    }, [])
+    return (
+        <>
+          <Helmet>
+            <title>Discord</title>
+            <meta name="description" content="My Discord page through Helmet" />
+          </Helmet>
+            <DiscordBot />
+            {
+                userData ? <DiscordUser userData={userData}/> :
+                <a href={oauthURL}>
+                    <button>
+                        Login to Discord
+                    </button>
+                </a>
+            }
+        </>
+    )
 }
-
-export default Discord;
