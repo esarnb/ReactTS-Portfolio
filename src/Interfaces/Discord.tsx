f4a17dac3c456e45cb5eb75e715fb361060c8443
esarnb <esarnb@gmail.com>
2022-04-24 14:53:45 +0000

    useReducer templating

--- src/Interfaces/Discord.tsx
+++ src/Interfaces/Discord.tsx
@@ -1,35 +1,66 @@
 export interface userObj {
-    id: string,
-    username: string,
-    avatar: string,
-    discriminator: string,
-    public_flags: number,
-    flags: number,
-    banner: string,
-    banner_color: string,
-    accent_color: number,
-    locale: string,
-    mfa_enabled: boolean,
-    premium_type: number
+  id: string,
+  username: string,
+  avatar: string,
+  discriminator: string,
+  public_flags: number,
+  flags: number,
+  banner: string,
+  banner_color: string,
+  accent_color: number,
+  locale: string,
+  mfa_enabled: boolean,
+  premium_type: number
+}
+
+export enum DiscordActionEnum {
+  setCode = "SETCODE",
+  clearCode = "CLEARCODE"
+}
+
+export interface DiscordActionI {
+  type: DiscordActionEnum,
+  payload: string
+}
+
+export interface DiscordAuthState {
+  code: string | null
+}
+
+export function DiscordReducer(state: DiscordAuthState, action: DiscordActionI) {
+  const {type, payload} = action;
+  switch (type) {
+    case DiscordActionEnum.setCode:
+      return {
+        ...state, value: payload 
+      }
+
+    case DiscordActionEnum.clearCode:
+      return {
+        ...state, value: null 
+      }
+
+    default: return state;
+
+  }
 }
 
 
-export interface command {
-    help: {
-      desc: string,
-      guild: boolean, 
-      locked: boolean,
-      name: string, 
-      owner: boolean, 
-      type: string,
-      usage: string,
-    }
-  }
-  
+// export interface command {
+//   help: {
+//     desc: string,
+//     guild: boolean,
+//     locked: boolean,
+//     name: string,
+//     owner: boolean,
+//     type: string,
+//     usage: string,
+//   }
+// }
+
 export interface botData {
-    user: any, // discord User Object
-    users: number,
-    servers: number,
-    commands: any // command Object
-  }
-  
+  user: any, // discord User Object
+  users: number,
+  servers: number,
+  commands: any // command Object
+}
--- src/Pages/Discord/Discord.tsx
+++ src/Pages/Discord/Discord.tsx
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { useSearchParams } from 'react-router-dom'
-import {  userObj  } from "../../Interfaces/Discord";
+import { DiscordActionEnum, DiscordReducer, userObj } from "../../Interfaces/Discord";
 import DiscordUser from "../../Components/DiscordUser/DiscordUser";
 import DiscordBot from "../../Components/DiscordBot/DiscordBot";
 import "./Discord.css";
@@ -12,32 +12,43 @@ export default function Discord() {
     const code = searchParams.get('code');
 
     const [userData, setUserData] = useState<userObj>();
+    const [state, dispatch] = useReducer(DiscordReducer, { code: null });
     const oauthURL = "https://discord.com/api/oauth2/authorize?client_id=343831827963838475&redirect_uri=https%3A%2F%2Fesarnb.com%2Fdiscord&response_type=code&scope=identify";
 
     useEffect(() => {
-      console.log("CODE", code);
-      if (code) {
-            fetch("https://api.esarnb.com/discord?code=" + code).then((res) => res.json()).then((data: userObj) => {
-                console.log("DATA", data);
+        console.log("State", state);
+        console.log("Code", code);
+
+        if (state.code || code) {
+            if (!state.code && code) {
+                dispatch({ type: DiscordActionEnum.setCode, payload: code });
+                console.log("EXECUTED DISPATCH", DiscordActionEnum.setCode, code);
+                
+            }
+            searchParams.delete('code');
+            console.log(state);
+            
+            fetch("https://api.esarnb.com/discord?code=" + (state.code ?? code)).then((res) => res.json()).then((data: userObj) => {
                 setUserData(data);
             }).catch(err => console.error(err));
         }
     }, [])
     return (
         <>
-          <Helmet>
-            <title>Discord</title>
-            <meta name="description" content="My Discord page through Helmet" />
-          </Helmet>
+            <Helmet>
+                <title>Discord</title>
+                <meta name="description" content="My Discord page through Helmet" />
+            </Helmet>
             <DiscordBot />
             {
-                userData ? <DiscordUser userData={userData}/> :
+                userData ? <DiscordUser userData={userData} /> :
                 <a href={oauthURL}>
                     <button>
                         Login to Discord
                     </button>
                 </a>
             }
+            <p>{state.code}</p>
         </>
     )
 }
